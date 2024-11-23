b7ba031e92a4fdcae906123d97e2a69298d025d1
Rizzdev984 <anjai1233211@gmail.com>
2024-11-23 13:26:00 +0700

    Add files via upload

--- app/docs/layout.js
+++ app/docs/layout.js
@@ -1,1 +1,37 @@
+// app/docs/layout.js
+// custom
 
+export const metadata = {
+  title: "Celestial APIs",
+  description: "Rest APIs Made With🔥",
+  keywords: "Api",
+  openGraph: {
+    title: "Celestial APIs",
+    description:
+      "Free Rest APIs Made With🥰",
+    url: "https://api.celestial.us.kg",
+    type: "website",
+    images: [
+      {
+        url: "https://pomf2.lain.la/f/frd1xzm6.jpg",
+        width: 800,
+        height: 600,
+        alt: "anjing",
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
