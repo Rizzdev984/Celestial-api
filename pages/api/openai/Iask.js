97046d795a74685c616ed114b590cb25fb26439f
Rizzdev984 <anjai1233211@gmail.com>
2024-11-24 12:19:41 +0700

    Update Iask.js

--- pages/api/openai/Iask.js
+++ pages/api/openai/Iask.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: "Parameter Text Tidak Di isi"})
   }
   try {
-     const iaskk = await iask(text)
+     const iaskk = await pencarianTeks(text)
      return res.status(200).json({
        status: true,
        author: "Celestial",
